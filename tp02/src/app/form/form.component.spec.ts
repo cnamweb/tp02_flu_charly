5caea2778b7b700edc94d7508121a412ebf197a8
Charly <charly.flu.67@gmail.com>
2024-10-13 18:53:19 +0200

    Huge leap forward, seems to work so far

---
+++ tp02/src/app/form/form.component.spec.ts
@@ -0,0 +1,24 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormComponent } from './form.component';
+import {CommonModule} from '@angular/common';
+
+describe('FormComponent', () => {
+  let component: FormComponent;
+  let fixture: ComponentFixture<FormComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [FormComponent, 
+        CommonModule]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(FormComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
